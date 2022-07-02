949f2de7c77069c39b4ea95ed6f4737f50842ae1
Rahpael Anderegg <raphael.anderegg@bitforge.ch>
2022-07-02 11:47:59 +0200

    set up simple camera system

--- BoxInABox/Assets/pixlhero/Scripts/PixlheroPlayer.cs
+++ BoxInABox/Assets/pixlhero/Scripts/PixlheroPlayer.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PixlheroInput))]
+[RequireComponent(typeof(Orienter))]
 public class PixlheroPlayer : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
 
-    private PixlheroInput input;
+
+    private PixlheroInput _input;
+
+    private Orienter _orienter;
 
     private void Awake() {
-        input = GetComponent<PixlheroInput>();
+        _orienter = GetComponent<Orienter>();
+        _input = GetComponent<PixlheroInput>();
     }
 
     private void Update() {
@@ -19,7 +24,8 @@ public class PixlheroPlayer : MonoBehaviour
     }
 
     private void Move(){
-        var moveDir = input.GetMoveDirection() * Time.deltaTime * speed;
-        transform.Translate(new Vector3(moveDir.x, 0, moveDir.y), Space.Self);
+        var inputDir = _input.GetMoveDirection() * Time.deltaTime * speed;
+        var moveDir = Camera.main.transform.TransformDirection(new Vector3(inputDir.x,  inputDir.y, 0f));
+        transform.Translate(moveDir, Space.World);
     }
 }
