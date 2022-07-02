ebf88353cab405e4bf7d71f680394bd4821ddabd
abe <abe@lightwits.localdomain>
2022-07-02 11:58:16 +0200

    Split out BoxItemState from BoxItemMapping.

--- BoxInABox/Assets/GlobalScripts/BoxItemState.cs
+++ BoxInABox/Assets/GlobalScripts/BoxItemState.cs
@@ -1,30 +1,6 @@
-using UnityEngine;
-using UnityEngine.Assertions;
-using System.Collections.Generic;
-
-public class BoxItemState : MonoBehaviour {
-    [System.Serializable]
-    public class ItemAndGameObject {
-        public Item item;
-        public GameObject prefab;
-    }
-
-    [SerializeField]
-    public List<ItemAndGameObject> ItemPrefabs;
+public class BoxItemState {
     // The item that is contained in the box to be transfered into the next minigame
     public Item HeldItem{ get; set; }
-    public GameObject GetHeldItemPrefab() {
-        foreach (ItemAndGameObject itemAndGameObject in ItemPrefabs) {
-            Assert.AreEqual(itemAndGameObject.item,
-                itemAndGameObject.prefab.GetComponent<ItemIdentifier>().item,
-                "");
-            if (itemAndGameObject.item == HeldItem) {
-                GameObject result = itemAndGameObject.prefab;
-                return result;
-            }
-        }
-        throw new System.Exception("No prefab found for item " + HeldItem);
-    }
 
     private BoxItemState() {}  
     private static BoxItemState _instance = null;  
