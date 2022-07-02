2d7a5052b660d826ed4967924afd2c5ad67afdf3
abe <abe@lightwits.localdomain>
2022-07-02 20:26:45 +0200

    Start adding bandaid particles.

--- BoxInABox/Assets/Abe/Scripts/BoxAnim.cs
+++ BoxInABox/Assets/Abe/Scripts/BoxAnim.cs
@@ -21,6 +21,10 @@ public class BoxAnim : MonoBehaviour
         for (int i = 0; i < lids.Count; i++)
         {
             float alpha = Mathf.Clamp(open, 0, 1);
+            if (i % 2 == 0)
+            {
+                alpha =  Mathf.Clamp(open+0.3f, 0, 1);
+            }
             float angle = openAngles[i] * alpha + closedAngles[i] * (1 - alpha);
             lids[i].transform.localEulerAngles =
                 (i % 2 == 0 ? Vector3.up : Vector3.right) * angle;
