9e2a061e76bd8f5421af3c8e0d89e4e1e39f1b3a
Ruben Nuñez <ruben.nunez@live.de>
2022-07-02 15:10:43 +0200

    scene fog for my scene added

--- BoxInABox/Assets/GlobalScripts/BoxItemState.cs
+++ BoxInABox/Assets/GlobalScripts/BoxItemState.cs
@@ -2,16 +2,13 @@ public class BoxItemState {
     // The item that is contained in the box to be transfered into the next minigame
     public Item HeldItem{ get; set; }
 
-    private BoxItemState() {}  
-    private static BoxItemState _instance = null;  
-    public static BoxItemState Instance {  
-        get {  
-            if (_instance == null) {  
-                _instance = new BoxItemState();  
-            }  
-            return _instance;  
-        }  
-    }
+    private BoxItemState()
+    {
+        
+    }  
+    
+    private static BoxItemState _instance;  
+    public static BoxItemState Instance => _instance ??= new BoxItemState();
 
     public enum Item {
         // add items
