decf6342029099b73966a2a25334df4747c74b6a
Rahpael Anderegg <raphael.anderegg@bitforge.ch>
2022-07-03 11:47:25 +0200

    added script to fix error

---
+++ BoxInABox/Assets/Dareios/EventFixScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this on your animator
+public class EventFixScript : MonoBehaviour
+{
+    public void StepLeft(){
+        // do nothing
+    }
+
+    public void StepRight(){
+        // do nothing
+    }
+}
