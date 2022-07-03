cd8f45707556c7fde9d2e89f96f475b740d7744b
Rahpael Anderegg <raphael.anderegg@bitforge.ch>
2022-07-03 09:27:59 +0200

    added level colors

--- BoxInABox/Assets/pixlhero/Scripts/PixlheroLevel.cs
+++ BoxInABox/Assets/pixlhero/Scripts/PixlheroLevel.cs
@@ -19,7 +19,22 @@ public class PixlheroLevel : MonoBehaviour
             BoxItemState.Instance.HeldItem = fallbackLevel;
         }
 
+        Camera.main.backgroundColor = GetLevelColor(BoxItemState.Instance.HeldItem);
+
         var levelPrefab = _levelMapping.GetHeldItemPrefab();
         Instantiate(levelPrefab, transform);
+    }
+
+    private Color GetLevelColor(BoxItemState.Item item){
+        return item switch
+        {
+            BoxItemState.Item.Banana => new Color(0.85f, 0.84f, 0.68f),
+            BoxItemState.Item.BaseItem => Color.white,
+            BoxItemState.Item.Motorcycle => new Color(0.52f, 0.54f, 0.62f),
+            BoxItemState.Item.Mallet => new Color(0.64f, 0.54f, 0.48f),
+            BoxItemState.Item.ButterflyCatchingNet => new Color(0.48f, 0.66f, 0.70f),
+            BoxItemState.Item.HourGlass => new Color(0.74f, 0.70f, 0.54f),
+            _ => Color.white,
+        };
     }
 }
