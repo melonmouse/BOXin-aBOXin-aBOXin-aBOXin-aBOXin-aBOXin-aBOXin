9aea8baec8b646807463c36bf8b58867fd87487d
abe <abe@lightwits.localdomain>
2022-07-03 09:50:29 +0200

    Make game rotate slowly.

--- BoxInABox/Assets/Abe/Scripts/InifiniteCamera.cs
+++ BoxInABox/Assets/Abe/Scripts/InifiniteCamera.cs
@@ -9,6 +9,10 @@ public class InifiniteCamera : MonoBehaviour
     public float wrap_to;
     public RandomRotation rotationManager;
     public bool wrapBack;
+
+    public float camera_angle = 0f;
+    public float camera_rotation_speed;
+
     void Update()
     {
         float height = transform.position.y;
@@ -34,6 +38,7 @@ public class InifiniteCamera : MonoBehaviour
         {
             SceneTransition.GoToRandomNextScene();
         }
-
+        camera_angle += camera_rotation_speed * Time.deltaTime;
+        transform.rotation = Quaternion.EulerAngles(Vector3.up * camera_angle);
     }
 }
